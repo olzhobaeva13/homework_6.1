826fd29eeeb411073d34d823a0828e348bfa7291
Avat <avatolzhobaeva@gmail.com>
2021-09-14 00:43:05 +0600

    updated

--- task3.py
+++ task3.py
@@ -2,6 +2,6 @@ from functools import reduce
 
 numbers = [4, 17, 3, 90, 28, 55]
 
-reduced_nums = reduce(lambda x, y: x*y, numbers)
-#TODO что за 3 аргумент
+reduced_nums = reduce(lambda x, y: x*y, numbers, 15) 
+
 print(reduced_nums)
--- task4.py
+++ task4.py
@@ -2,12 +2,10 @@ from functools import reduce
 
 palindromes = ['hello', 'sentences where punctuation', 45,'Able was I, ere I saw Elba', 34.0, 78.87, 'found', 'level', '12/11/21', 'radar','stats']
 
+# вытаскиваем палиндромы
 palindromes_lst = list(filter(lambda x: x == x[::-1], list(map(lambda x: str(x).lower().replace(' ', '').replace(',', ''), palindromes))))
 
+# объединяем в строку
 result = reduce(lambda x, y: x+y, palindromes_lst)
 
 print(result)
-
-
-
-
